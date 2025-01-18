201428ed07602622f86f296ffc37c07861334b07
mariellajeitani <jeitanimariella@gmail.com>
2025-01-18 14:11:07 +0200

    add bootdtrap and primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule,IconFieldModule,InputIconModule,InputTextModule,FloatLabelModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'movies-library-app';
+  value3=''
 }
