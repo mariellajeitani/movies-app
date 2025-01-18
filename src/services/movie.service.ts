86d957bbd47be598596a9378955dec7bd45e39e9
mariellajeitani <jeitanimariella@gmail.com>
2025-01-18 19:24:06 +0200

    changes

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -12,5 +12,8 @@ export class MovieService {
   getMovies(): Observable<any> {
     return this.http.get<any>('https://api.themoviedb.org/3/movie/now_playing?api_key=9e393b3e456e1b90065cd1406251c131');  
   }
+  getMovieDetails(id:any): Observable<any> {
+    return this.http.get<any>(`https://api.themoviedb.org/3/movie/${id}?api_key=9e393b3e456e1b90065cd1406251c131`);  
+  }
 }
 
