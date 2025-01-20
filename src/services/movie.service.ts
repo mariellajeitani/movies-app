64016cd0a7c3baed788a9e397291d1a51b0be256
mariellajeitani <jeitanimariella@gmail.com>
2025-01-20 19:57:42 +0200

    add memory cache

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,23 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
+  private cache: Map<string, any> = new Map();
 
   constructor(private http: HttpClient) {}
 
-  getMovies(title:string): Observable<any> {
-    // return this.http.get<any>('https://api.themoviedb.org/3/movie/now_playing?api_key=9e393b3e456e1b90065cd1406251c131');  
-    return this.http.get<any>(`http://localhost:8081/movies/searchMovies?query=${title}`);  
+  // Fetch movies by title with optional caching
+  getMovies(title: string): Observable<any> {
+    if (this.cache.has(`search:${title}`)) {
+      console.log(`Serving movies for "${title}" from cache.`);
+      return of(this.cache.get(`search:${title}`));
+    }
 
+   // return this.http.get<any>(`https://api.themoviedb.org/3/movie/${id}?api_key=9e393b3e456e1b90065cd1406251c131`);  
+    return this.http
+      .get<any>(`http://localhost:8081/movies/searchMovies?query=${title}`)
+      .pipe(
+        tap((data) => {
+          this.cache.set(`search:${title}`, data); 
+        })
+      );
   }
-  getMovieDetails(id:any): Observable<any> {
-    // return this.http.get<any>(`https://api.themoviedb.org/3/movie/${id}?api_key=9e393b3e456e1b90065cd1406251c131`);  
-    return this.http.get<any>(`http://localhost:8081/movies/getMovieDetails/${id}`);
-    
+
+  getMovieDetails(id: any): Observable<any> {
+    if (this.cache.has(`details:${id}`)) {
+      console.log(`Serving details for movie ID ${id} from cache.`);
+      return of(this.cache.get(`details:${id}`));
+    }
+   // return this.http.get<any>(`https://api.themoviedb.org/3/movie/${id}?api_key=9e393b3e456e1b90065cd1406251c131`);  
+    return this.http
+      .get<any>(`http://localhost:8081/movies/getMovieDetails/${id}`)
+      .pipe(
+        tap((data) => {
+          this.cache.set(`details:${id}`, data); 
+        })
+      );
   }
 }
+
 
